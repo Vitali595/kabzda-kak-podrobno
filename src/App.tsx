3b98c40f66f274e5b11e2097a3c3cfac8eefe537
Vitali595 <vitaliy.kovalchuk.2001@gmail.com>
2021-04-04 16:21:05 +0300

    15 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {OnOff} from "./components/OnOff/OnOff";
 import {Accordion} from "./components/Accordion/Accordion";
 import {UncontrolledAccordion} from "./components/UncontrolledAccordion/UncontrolledAccordion";
 import {UncontrolledOnOff} from "./components/UncontrolledOnOff/UncontrolledOnOff";
+import {ItemType} from "./components/Select/Select";
 
 function App() {
     console.log("App rendering")
@@ -14,6 +15,12 @@ function App() {
     let [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false);
     let [switchOn, setSwitchOn] = useState<boolean>(false);
 
+    let [items, setOption] = useState<ItemType[]>([
+        {title: "None", value: 1},
+        {title: "Minsk", value: 2},
+        {title: "Moscow", value: 3},
+    ])
+
     return (
         <div className={"App"}>
             {/*<PageTitle title={"This is App components"}/>
@@ -21,7 +28,13 @@ function App() {
             Article 1
             <UncontrolledAccordion titleValue={"Menu"}/>
             <UncontrolledAccordion titleValue={"User"}/>
-            <Accordion titleValue={"Menu1"} collapsed={accordionCollapsed} onChange={() => {setAccordionCollapsed(!accordionCollapsed)}}/>
+            <Accordion
+                titleValue={"Menu1"}
+                collapsed={accordionCollapsed}
+                onChange={() => {setAccordionCollapsed(!accordionCollapsed)}}
+                items={[]}
+                onClick={x=>x}
+            />
             Article 2
             <UncontrolledRating onChange={setRatingValue}/>
             <Rating value={ratingValue} onClick={setRatingValue}/>
--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -10,11 +10,23 @@ export default {
 }
 
 const callback = action("accordion mode change event fired");
+const onClickCallback = action("some item was clicked");
 
-export const MenuCollapsedMode = () => <Accordion titleValue={"Menu"} collapsed={true} onChange={callback}/>
-export const UserUnCollapsedMode = () => <Accordion titleValue={"Users"} collapsed={false} onChange={callback}/>
+export const MenuCollapsedMode = () => <Accordion titleValue={"Menu"} collapsed={true} onChange={callback} items={[]}
+                                                  onClick={onClickCallback}/>
+export const UserUnCollapsedMode = () => <Accordion titleValue={"Users"} collapsed={false} onChange={callback} items={[
+    {title: "Dimych", value: 1},
+    {title: "Valera", value: 2},
+    {title: "Vitali", value: 3},
+    {title: "Artem", value: 4}
+]} onClick={onClickCallback}/>
 
 export const ModeChanging = () => {
     const [value, setValue] = useState<boolean>(true);
-    return <Accordion titleValue={"Users"} collapsed={value} onChange={() => setValue(!value)}/>
+    return <Accordion titleValue={"Users"} collapsed={value} onChange={() => setValue(!value)} items={[
+        {title: "Dimych", value: 1},
+        {title: "Valera", value: 2},
+        {title: "Vitali", value: 3},
+        {title: "Artem", value: 4}
+    ]} onClick={(value) => {alert(`user with ID ${value} should be happy`)}}/>
 }
